f88fb025f6e7191fff7c4656225649d54bc0a23c
JFuton847 <jimmyfuton847@gmail.com>
2024-11-02 12:38:46 -0500

    add some user routing

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const mainRouter = require("./routes/index");
 
 const app = express();
 const { PORT = 3001 } = process.env;
 
+mongoose.set("strictQuery", true);
+
 mongoose
-  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .connect("mongodb://127.0.0.1:27017/wtwr_db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to DB");
   })
   .catch(console.error);
+
+app.use(express.json());
+app.use("/", mainRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
