cab07ffd83a123842a665e8873e56ae57c56682f
JFuton847 <jimmyfuton847@gmail.com>
2024-11-23 13:07:44 -0600

    add select in two different areas of user schema

--- models/user.js
+++ models/user.js
@@ -31,7 +31,8 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: [false, "Password is required."],
+    required: [true, "Password is required."],
+    select: false,
   },
 });
 
@@ -39,18 +40,20 @@ userSchema.statics.findUserByCredentials = function findUserByCredentials(
   email,
   password
 ) {
-  return this.findOne({ email }).then((user) => {
-    if (!user) {
-      return Promise.reject(new Error("Incorrect email or password"));
-    }
-    return bcrypt.compare(password, user.password).then((matched) => {
-      if (!matched) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
         return Promise.reject(new Error("Incorrect email or password"));
       }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("Incorrect email or password"));
+        }
 
-      return user;
+        return user;
+      });
     });
-  });
 };
 
 module.exports = mongoose.model("user", userSchema);
