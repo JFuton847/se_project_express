4185959690962d19c32b9f1e7ae47f911ca206a9
JFuton847 <jimmyfuton847@gmail.com>
2025-01-28 20:52:57 -0600

    remove next from line 41 on app.js)

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.post("/signup", createUser);
 app.use(requestLogger);
 app.use("/", mainRouter);
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   res
     .status(err.status || 500)
     .json({ message: err.message || "Server Error" });
